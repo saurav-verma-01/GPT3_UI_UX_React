67fffc4ac507c4f2a39c77cc0de60361060717e1
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-04-16 22:56:18 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Article } from "./Article/Article";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Brands } from "./Brands/Brands";
+export { default as Cta } from "./Cta/Cta";
+export { default as Feature } from "./Feature/Feature";
